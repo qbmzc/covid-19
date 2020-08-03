9005a28b55af5d832e3dd1a463dfd0059f58ec1a
song <congco@foxmail.com>
2020-08-03 14:45:05 +0800

    111

--- src/covid_baidu_news_to_search.py
+++ src/covid_baidu_news_to_search.py
@@ -8,6 +8,7 @@ import random
 from docx import Document
 from docx.shared import Inches
 
+
 # 肺炎
 # 新冠肺炎国外疫情
 
@@ -59,6 +60,7 @@ def get_data(url):
 # lastUpdateTime = str(datetime.date.today())  #
 directory = "/data/Space/covid/baidu/"  # 定义数据保存路径
 
+
 # 图片下载
 def download_img(image_url):
     headers = {
@@ -80,6 +82,16 @@ def download_img(image_url):
 
 
 def get_news(req_url, eventDescription, eventTime):
+    headers = {
+        'Connection': 'keep-alive',
+        'Pragma': 'no-cache',
+        'Cache-Control': 'no-cache',
+        'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/81.0.4044.122 Safari/537.36',
+        'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9',
+        'Accept-Encoding': 'gzip, deflate',
+        'Accept-Language': 'zh-CN,zh;q=0.9,en-US;q=0.8,en;q=0.7,zh-TW;q=0.6',
+        'Content-Type': 'application/json'
+    }
     data = get_data(req_url)
     a_list = data.find_all('span', class_='bjh-p')
     news_content = ''
@@ -89,19 +101,18 @@ def get_news(req_url, eventDescription, eventTime):
     news_title = eventDescription
     time_local = time.localtime(int(eventTime))
     news_date = time.strftime("%Y-%m-%d %H:%M:%S", time_local)
+    # 保存到elasticsearch
     # 创建word文档
-    doc = Document()
-    doc.add_heading(news_title)
-    doc.add_paragraph(news_date)
-    doc.add_paragraph(news_content)
-    for img in data.find_all('img', class_='large'):
-        ssrc = img.get('src')
-
-        print(ssrc)
-
-        download_img(ssrc)
-        doc.add_picture('./img.jpg', width=Inches(5.0), height=Inches(5.0))
-    doc.save(directory + news_date + "_" + news_title + '.docx')
+    news = {
+        'title': str(news_title),
+        'newDate': str(news_date),
+        'content': str(news_content),
+        'category': 'news'
+    }
+    print(news)
+    search_url = 'http://127.0.0.1:15002/covid/news/save'
+    resp = requests.post(url=search_url, data=json.dumps(news), headers=headers)
+    print(resp.status_code)
 
 
 if __name__ == '__main__':
@@ -110,5 +121,5 @@ if __name__ == '__main__':
         eventUrl = urls['eventUrl']
         eventDescription = urls['eventDescription']
         eventTime = urls['eventTime']
-        print(eventUrl+eventDescription+eventTime)
+        print(eventUrl + eventDescription + eventTime)
         get_news(eventUrl, eventDescription, eventTime)
